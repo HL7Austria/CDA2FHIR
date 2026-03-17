71c93bbef33892bb947d81cd0d0d46f66ac4dd5d
gabriel0316 <gabriel.kleinoscheg@gmx.at>
2026-03-17 15:37:49 +0100

    Map display names; relax validation

--- scripts/convert_and_validate.py
+++ scripts/convert_and_validate.py
@@ -59,7 +59,8 @@ for config in config_json:
                 "-show-message-ids",
                 "-allow-example-urls", "true",
                 "-advisor-file", f"{INPUT_DIR}/{directory}/advisor.json",
-                "-extension", "any"
+                "-extension", "any",
+                "-display-issues-are-warnings"
             ], stdout=log_file, stderr=subprocess.STDOUT)
 
             if result.returncode != 0:
@@ -77,7 +78,8 @@ for config in config_json:
                 "-show-message-ids",
                 "-allow-example-urls", "true",
                 "-advisor-file", f"{INPUT_DIR}/{directory}/advisor.json",
-                "-extension", "any"
+                "-extension", "any",
+                "-display-issues-are-warnings"
             ], stdout=log_file, stderr=subprocess.STDOUT)
 
             if result.returncode != 0:
