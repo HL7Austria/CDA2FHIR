e67a807604c43b829871c6e58ddaf1e1a1af2eda
Renner <constantin.renner@elga.gv.at>
2026-04-01 15:09:38 +0200

    print log files

--- check_validation_result.sh
+++ check_validation_result.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-(grep -riE "failure|exception" validation/* > final_validation_check.log)
+grep -riE "failure|exception" validation/* > final_validation_check.log
 RESULT="$?"
 if [[ "$RESULT" != "0" ]]; then
     echo -e "No 'FAILURE' and/or 'exception' could be found in validation log files."
     exit 0
 else
-    echo -e "'FAILURE' and/or 'exception' could be found in validation log files."
+    echo -e "'FAILURE' and/or 'exception' could be found in the following validation log files:"
+    grep -riEl "failure|exception" validation/*
     exit 20
 fi
