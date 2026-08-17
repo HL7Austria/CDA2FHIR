611709a304ad58607874f9ce5863d8dfccd360bb
Renner <constantin.renner@elga.gv.at>
2026-08-17 13:03:40 +0200

    add code to print bundle with error to artifact

--- check_validation_result.sh
+++ check_validation_result.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-grep -riE "failure|exception" validation/* > final_validation_check.log
-RESULT="$?"
-if [[ "$RESULT" != "0" ]]; then
+FAILED_BUNDLES="validation/failed_bundles.txt"
+
+# empty it first so grep below never reads a previous run's result
+: > "$FAILED_BUNDLES"
+
+# validation/<dir>/<basename>.val.<xml|json>.<log|html>
+#   -> output/<dir>/<basename>.fhir.<xml|json>
+grep -rliE "failure|exception" validation/* \
+    | sed -E -e 's#^validation/#output/#' -e 's#\.val\.(xml|json)\.(log|html)$#.fhir.\1#' \
+    | sort -u > "$FAILED_BUNDLES"
+
+if [[ -s "$FAILED_BUNDLES" ]]; then
+    echo -e "'FAILURE' and/or 'exception' could be found for the following FHIR bundles:"
+    cat "$FAILED_BUNDLES"
+    exit 20
+else
     echo -e "No 'FAILURE' and/or 'exception' could be found in validation log files."
     exit 0
-else
-    echo -e "'FAILURE' and/or 'exception' could be found in the following validation log files:"
-    grep -riEl "failure|exception" validation/*
-    exit 20
 fi
